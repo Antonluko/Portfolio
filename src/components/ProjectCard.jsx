19bf70385dbcbde6ce131152a751e4d7f2043f47
Anton <alukosevicus@inbox.lv>
2026-03-24 22:54:18 +0000

    Added image & tech to project card component

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,9 +1,10 @@
-function ProjectCard({ title, description, link}) {
+function ProjectCard({ title, description, link, tech, image}) {
     return (
         <div className="project-card"> 
+            <img src={image} alt={title} className="project-image" />
             <h3> {title} </h3>
             <p> {description} </p>
-
+            <p className="project-tech"> Tech: {tech} </p>
             <a href={link} target="_blank"  rel="noreferrer">
                 <button> View Project</button>
             </a>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,7 @@
 import ProjectCard from "./ProjectCard"
-
+import portfolioImage from "../assets/portfolio.png"
+import workoutImage from "../assets/workout.png"
+import bookImage from "../assets/book.png"
 function Projects() {
   return (
     <section id="projects" classNamer="projects">
@@ -10,19 +12,25 @@ function Projects() {
         <ProjectCard 
         title="Portfolio Website"
         descritption="A personal protfolio built with React and Vite"
+        tech="React, CSS, Vite"
         link="https://github.com/Antonluko/Portfolio"
+        image={portfolioImage}
         />
 
         <ProjectCard
         title="Workout Tracker"
         description="A frontend app for tracking workouts and progress"
+        tech="React, Javascript, HTML, SCSS, SQL, phpMyAdmin,"
         link="https://github.com/" // Will need to be updated 
+         image={workoutImage}
         />
 
         <ProjectCard 
         title="Book Managment App"
         descriotion="A simple app to organise and manage books."
+        tech="Kotlin, Android"
         link="https://github.com/" // Will need to be updated
+         image={bookImage}
         />
 
       </div>
