767e60d84fbc6ac3c49217df752cde9ed630b18a
Anton <alukosevicus@inbox.lv>
2026-03-24 21:56:20 +0000

    Refactored Contact me component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,17 @@
 function Contact() {
   return (
-    <section id="contact">
+    <section id="contact" className="contact">
       <h2>Contact</h2>
-      <p>You can contact me here in the future</p>
+      <p>You can contact me here</p>
+      <ul className="contact-list">
+        <li>Email: alukosevicus@inbox.lv</li>
+        <li> 
+          Github: <a href="https://github.com/Antonluko" target="_blank"> github.com/Antonluko</a>
+        </li>
+        <li> LinkedIn: <a href="https://www.linkedin.com/in/anton-lukosevicus">
+        https://www.linkedin.com/in/anton-lukosevicus</a>
+        </li>
+      </ul>
     </section>
   )
 }
