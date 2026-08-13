34ec423ba0f3f59cbdfac53992387b7189c80592
Anton <alukosevicus@inbox.lv>
2026-08-13 19:00:49 +0100

    Refactored Home & Styling
    - Changed text.
    - Added template image.
    - Changed styling of Home & Sections

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,16 @@
+import avatar from "../assets/avatar.avif";
 function Home() {
     return (
         <section id="home" className="home"> 
-            <h1>Welcome, I'm Anton a Software Developer</h1> <br/>
-            <a href="#projects">
+        <div className="home-content"> 
+            <h1>Welcome, I'm <span className="highlight">Anton </span> a <br/>  <span className="highlight">Software Developer </span></h1> <br/>
+              <a href="#projects">
                 <button>View Projects</button>
             </a>
+         
+            </div>
+               <img src={avatar} alt="avatar" />
+          
         </section>
     )
 }
